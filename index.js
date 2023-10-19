118b357dfed9d4348e3c8101df96d1ad0468586c
Mohammad Dinar <web.dev.dinar@gmail.com>
2023-10-19 16:02:05 -0700

    I have  successfully got the datas of slider from mongodb database and also have successfully sent the response to the client site

--- index.js
+++ index.js
@@ -1,16 +1,13 @@
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
-
 
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
-
-
 
 const uri = `mongodb+srv://${process.env.OWNER_NAME}:${process.env.OWNER_PASS}@cluster0.u8ojnwq.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -19,22 +16,36 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
     await client.connect();
+    const AddSlider = client.db("EntoMedu").collection("AddSlider");
+
+    app.get("/brand", async (req, res) => {
+        const cursor = AddSlider.find({});
+        const add = await cursor.toArray();
+        res.send(add)
+        });
+        app.get("/brand/:brandName",async(req, res)=>{
+            const brandName = req.params.brandName;
+            const query = {brandName : brandName}
+            const brandData = await AddSlider.findOne(query)
+            res.send(brandData)
+        })
+
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
   }
 }
 run().catch(console.dir);
-
-
 
 app.get("/", (req, res) => {
   res.send("My server is running");
