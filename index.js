1fdcf8c6894ff0a71649642278ab1cb9f42edaaf
Mohammad Dinar <web.dev.dinar@gmail.com>
2023-10-21 21:20:49 -0700

    server code has been updated

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    await client.connect();
+    // await client.connect();
     const AddSlider = client.db("EntoMedu").collection("AddSlider");
     const AddProduct = client.db("EntoMedu").collection("AddProduct");
     const cartCollection = client.db("EntoMedu").collection("cart");
@@ -96,10 +96,10 @@ async function run() {
             res.send(result)
         })
 
-    await client.db("admin").command({ ping: 1 });
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
-    );
+    // await client.db("admin").command({ ping: 1 });
+    // console.log(
+    //   "Pinged your deployment. You successfully connected to MongoDB!"
+    // );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
